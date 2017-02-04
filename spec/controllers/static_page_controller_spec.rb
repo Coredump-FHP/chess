e620843f7b7bf9640d78ad6ddfedb7b834df06c7
Sijia Davis <arwen@ksu.edu>
2017-02-04 23:18:11 +0000

    Initial commit

---
+++ spec/controllers/static_page_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+  describe "static_pages#home action" do
+    it "should successfuly show the page" do
+      get :home
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
