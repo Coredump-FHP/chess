99fcac59664692494d145d62b99f4010f2ea8024
Elson Mathew <Mathew@Elsons-MacBook-Pro.local>
2017-02-12 18:48:39 -0500

    Added belongs to associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,7 @@
 class Game < ApplicationRecord
   validates :name, presence: true, length: { minimum: 1 }
   has_many :pieces
-  has_many :players
+  belongs_to :player_1, class_name: 'Player'
+  belongs_to :player_1, class_name: 'Player'
+  belongs_to :winning_user, class_name: 'Player'
 end
