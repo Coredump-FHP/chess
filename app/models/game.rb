22be486be06d1c6413630b494e003d395c6bbb26
Elson Mathew <Mathew@Elsons-MacBook-Pro.local>
2017-02-12 23:03:30 -0500

    Added optional: true to games model associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
   validates :name, presence: true, length: { minimum: 1 }
   has_many :pieces
-  belongs_to :player_1, class_name: 'Player'
-  belongs_to :player_2, class_name: 'Player'
-  belongs_to :winning_player_id, class_name: 'Player'
+  belongs_to :player_1, class_name: 'Player', optional: true
+  belongs_to :player_2, class_name: 'Player', optional: true
+  belongs_to :winning_player, class_name: 'Player', optional: true
 end
