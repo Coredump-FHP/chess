1db0687fab0a6e9ced72e0fdc4c2142a339cf2f5
Elson Mathew <Mathew@Elsons-MacBook-Pro.local>
2017-02-14 00:25:57 -0500

    Add sti

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,12 @@
 class Piece < ApplicationRecord
   belongs_to :player
   belongs_to :game
+
+  self.inheritance_column = :type
+
+  # We will need a way to know which animals
+  # will subclass the Animal model
+  def self.type
+    %w(Pawn King Queen Rook Knight Bishop)
+  end
 end
