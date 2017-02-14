070935ac988cb0d50f0ec025deeb67c02333799d
Elson Mathew <Mathew@Elsons-MacBook-Pro.local>
2017-02-14 14:08:18 -0500

    Remove def self.type from pieces model

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,11 +1,4 @@
 class Piece < ApplicationRecord
   belongs_to :player
   belongs_to :game
-
-
-  # We will need a way to know which pieces
-  # will subclass the Piece model
-  def self.type
-    %w(Pawn King Queen Rook Knight Bishop)
-  end
 end
