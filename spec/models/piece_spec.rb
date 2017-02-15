e7d4d9953e942db5865a78a1e690418b500d8edb
sylyu <sylviah.yu@gmail.com>
2017-02-15 20:33:53 +0000

    added code back into  piece_spec.rb

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  # it 'has a valid factory' do
-  # FactoryGirl.build(:pawn)
-  # end
+  it 'has a valid factory' do
+   FactoryGirl.build(:piece)
+  end
 end
