f45ed51e514ed0152416aaf8067773cb67fbb57e
Sijia Davis <arwen@ksu.edu>
2017-02-16 22:09:12 +0000

    Finish the obstructed method

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,59 +2,41 @@ class Piece < ApplicationRecord
   belongs_to :player
   belongs_to :game
   
+  def obstructed?(sq)
+    # example of sq: [1, 2]
+    # x_coord of sq: sq[0], y_coor of sq: sq[1]
+    x_difference = self.x_coordinate - sq[0]
+    y_difference = self.y_coordinate - sq[1]
 
-def obstructed?(sq)
-  # example of sq: [1, 2]
-  # x_coord of sq: sq[0], y_coor of sq: sq[1]
-  x_difference = self.x_coordinate - sq[0]
-  y_difference = self.y_coordinate - sq[1]
+    @chess_in_between = []
+    # first, raise an argument error when self and sq have the same coordinates
+    if x_difference == 0 && y_difference == 0
+      raise ArgumentError.new("location to check has to be different")
+    elsif x_difference == 0 
+      # the two locations are on the same vertical line
+      # is there a chess piece on the same vertical line AND sits in between these two locations?
+      # the x_coordinates of this chess piece == self.x_coordinate
+      # the y_coordinates of the this chess piece should be in between (self.y_coordinate to sq[1])
 
-  # first, raise an argument error when self and sq have the same coordinates
-  if x_difference == 0 && y_difference == 0
-    raise ArgumentError.new("location to check has to be different")
-  elsif x_difference != y_difference
-    raise ArgumentError.new("x and y difference are not the same")
-  elsif x_difference == 0 
-
-    # the two locations are on the same vertical line
-    # is there a chess piece on the same vertical line AND sits in between these two locations?
-    # the x_coordinates of this chess piece == self.x_coordinate
-    # the y_coordinates of the this chess piece should be in between (self.y_coordinate to sq[1])
-
-
-  chess_in_between = Piece.where(x_coordinate: self.x_coordinate).where(y_coordinate: self.y_coordinate+1...sq[1])
-    if chess_in_between.empty? 
-      return false
-    else
-      return true
-    end
-
-  elsif y_difference == 0 # the two locations are on the same horizontal line
-    chess_in_between = Piece.where(y_coordinate: self.y_coordinate).where(x_coordinate :self.x_coordinate+1...sq[0])
-    if chess_in_between == []
-      return false
-    else
-      return true 
-    end
-
-  else # the two locations are on the same diagonal line
-    # three conditions to check:
-    # is the x_coord of the chess piece in between self.x_coordinate and sq[0]
-    # is the y_coord of the chess piece in between self.y_coordinate and sq[1]
-    # if the two conditions above are all satisfied, we also want to check 
-    chess_in_between = Piece.where(x_coordinate: self.x_coordinate+1...sq[0]) # making sure the x_coord of the chess piece in between self.x_coordinate and sq[0]
-                            .where(y_coordinate: self.y_coordinate+1...sq[1]) # making sure the y_coord of the chess piece in between self.y_coordinate and sq[1]
-                            .where((x_coordinate: - self.x_coordinate).eq(y_coordinate: - self.y_coordinate))
-                              
-    if chess_in_between == []
-      return false
-    else
-      return true 
+      @chess_in_between = Piece.where(x_coordinate: self.x_coordinate)
+                               .where(y_coordinate: self.y_coordinate+1...sq[1])
+    elsif y_difference == 0 # the two locations are on the same horizontal line
+      @chess_in_between = Piece.where(y_coordinate: self.y_coordinate)
+                               .where(x_coordinate: self.x_coordinate+1...sq[0])
+    elsif x_difference != y_difference
+      raise ArgumentError.new("x and y difference are not the same")
+    else # the two locations are on the same diagonal line
+      # three conditions to check:
+      # is the x_coord of the chess piece in between self.x_coordinate and sq[0]
+      # is the y_coord of the chess piece in between self.y_coordinate and sq[1]
+      # if the two conditions above are all satisfied, we also want to check 
+      @chess_in_between = Piece.where(x_coordinate: self.x_coordinate+1...sq[0]) # making sure the x_coord of the chess piece in between self.x_coordinate and sq[0]
+                              .where(y_coordinate: self.y_coordinate+1...sq[1]) # making sure the y_coord of the chess piece in between self.y_coordinate and sq[1]
+                              .where("(x_coordinate - #{self.x_coordinate}) = (y_coordinate- #{self.y_coordinate})")
+      
     end
     
+    !@chess_in_between.empty?
+
   end
-
-
-  
-
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -3,97 +3,102 @@ require 'rails_helper'
 RSpec.describe Piece, type: :model do
   
   describe "#obstructed?" do
-    # sq1 = [1, 7], sq2 = [3, 8] for example
+    # current_chess_piece being a Piece object having (x_coord: 1, y_coor: 2), sq2 = [3, 8] for example
     # what good inputs would be?
-    # sq1 and sq2 have the same x value(sq1[0] == sq2[0]), but not the same y value(sq1[1] != sq2[1])
-    # sq1 and sq2 have the same y value(sq1[1] == sq2[1]), but not the same x value(sq1[0] != sq2[0])
-    # sq1 and sq2 have the same x diff and y diff (sq1[0]-sq2[0] == sq1[1]-sq2[1])
+    # current_chess_piece and sq2 have the same x value(current_chess_piece.x_coordinate == sq2[0]), but not the same y value(current_chess_piece.y_coordinate != sq2[1])
+    # current_chess_piece and sq2 have the same y value(current_chess_piece.y_coordinate] == sq2[1]), but not the same x value(current_chess_piece.x_coordinate != sq2[0])
+    # current_chess_piece and sq2 have the same x diff and y diff (current_chess_piece.x_coordinate-sq2[0] == current_chess_piece.y_coordinate-sq2[1])
+    
+    before do 
+      @player1 = FactoryGirl.create(:player)
+      @player2 = FactoryGirl.create(:player)
+      @game = Game.create(name: 'my game', player_1: @player1, player_2: @player2)
+    end
+    
+    def create_chess(x, y)
+      Piece.create(x_coordinate: x, y_coordinate: y, player: @player1, game: @game)
+    end
     
     describe "When the inputs are invalid" do
       describe "Should raise an argument error" do    
-        it "If sq1 and sq2 have the same x and y coordinates" do
-          # setup the test data
-          sq1 = [0,1]
+        it "If current_chess_piece and sq2 have the same x and y coordinates" do
+          current_chess_piece = create_chess(0, 1)
+      
           sq2 = [0,1]
           # assertion
-          expect(sq1.obstructed?(sq2)).to raise_error ArgumentError
+          expect{current_chess_piece.obstructed?(sq2)}.to raise_error ArgumentError
         end
 
-        describe "If sq1 and sq2 have different x and y coordinates" do
+        describe "If current_chess_piece and sq2 have different x and y coordinates" do
           it "and the x differences and y differences are not the same" do
-            # setup the test data
-            sq1 = [0,5]
+            current_chess_piece = create_chess(0, 5)
+            
             sq2 = [1,2]
             # assertion
-            expect(sq1.obstructed?(sq2)).to raise_error ArgumentError
+            expect{current_chess_piece.obstructed?(sq2)}.to raise_error ArgumentError
           end  
         end    
       end
     end
     
     describe "When the inputs are valid" do
-      describe "If sq1 and sq2 have the same x coordinates" do
+      describe "If current_chess_piece and sq2 have the same x coordinates" do
         describe "but not the same y coordinates" do
           it "If there is a piece in between them, the result should be true" do
-            sq1 = [3, 3]
-            sq2 = [3, 5]
-            player1 = FactoryGirl.create(:player)
-            player2 = FactoryGirl.create(:player)
-            game = Game.create(name: 'my game', player_1: player1, player_2: player2)
-            chess_piece = Piece.create(x_coordinate: 3, y_coordinate:4, player: player1, game: game)
-        
-            expect(sq1.obstructed?(sq2)).to eq true
-          end
+            current_chess_piece = create_chess(3, 3)
           
-          it "If there is nothing in between them, the result should be false" do
-            sq1 = [3, 3]
             sq2 = [3, 5]
             
-            expect(sq1.obstructed?(sq2)).to eq false
+            chess_piece = create_chess(3, 4)
+        
+            expect(current_chess_piece.obstructed?(sq2)).to eq true
+          end
+          
+          
+          it "If there is nothing in between them, the result should be false" do
+            current_chess_piece = create_chess(3, 3)
+            sq2 = [3, 5]
+            
+            expect(current_chess_piece.obstructed?(sq2)).to eq false
           end
         end
       end
       
-      describe "If sq1 and sq2 have the same y coordinates" do
+      describe "If current_chess_piece and sq2 have the same y coordinates" do
         describe "but not the same x coordinates" do
           it "If there is a piece in between them, the result should be true" do
-            sq1 = [3, 3]
+            current_chess_piece = create_chess(3, 3)
+          
             sq2 = [5, 3]
-            player1 = FactoryGirl.create(:player)
-            player2 = FactoryGirl.create(:player)
-            game = Game.create(name: 'my game', player_1: player1, player_2: player2)
-            chess_piece = Piece.create(x_coordinate: 4, y_coordinate:3, player: player1, game: game)
+            chess_piece = create_chess(4, 3)
             
-            expect(sq1.obstructed?(sq2)).to eq true
+            expect(current_chess_piece.obstructed?(sq2)).to eq true
           end
           
           it "If there is nothing in between them, the result should be false" do
-            sq1 = [3, 3]
             sq2 = [5, 3]
+            current_chess_piece = create_chess(3, 3)
             
-            expect(sq1.obstructed?(sq2)).to eq false
+            expect(current_chess_piece.obstructed?(sq2)).to eq false
           end
         end
       end
       
-      describe "If sq1 and sq2 have different x and y coordinates" do
+      describe "If current_chess_piece and sq2 have different x and y coordinates" do
         describe "and the x differences and y differences are the same" do
           it "if there is a piece in between them, the result should be true" do
-            sq1 = [0, 0]
+            current_chess_piece = create_chess(0, 0)
             sq2 = [3, 3]
-            player1 = FactoryGirl.create(:player)
-            player2 = FactoryGirl.create(:player)
-            game = Game.create(name: 'my game', player_1: player1, player_2: player2)
-            chess_piece = Piece.create(x_coordinate: 1, y_coordinate:1, player: player1, game: game)
+            chess_piece = create_chess(1, 1)
             
-            expect(sq1.obstructed?(sq2)).to eq true
+            expect(current_chess_piece.obstructed?(sq2)).to eq true
           end
           
           it "if there is nothing in between them, the result should be false" do
-            sq1 = [0, 0]
+            current_chess_piece = create_chess(0, 0)
             sq2 = [3, 3]
           
-            expect(sq1.obstructed?(sq2)).to eq false
+            expect(current_chess_piece.obstructed?(sq2)).to eq false
           end
         end
       end
