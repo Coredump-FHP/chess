07c806db304ccbc946c568024c6baaa7a7a65c81
Sijia Davis <arwen@ksu.edu>
2017-02-16 23:12:16 +0000

    Modify the obstructed? method to take coordinates as params

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,16 +2,16 @@ class Piece < ApplicationRecord
   belongs_to :player
   belongs_to :game
 
-  def obstructed?(sq)
+  def obstructed?(destination_x, destination_y)
     # example of sq: [1, 2]
     # x_coord of sq: sq[0], y_coor of sq: sq[1]
-    x_difference = x_coordinate - sq[0]
-    y_difference = y_coordinate - sq[1]
+    x_difference = x_coordinate - destination_x
+    y_difference = y_coordinate - destination_y
 
     @chess_in_between = []
     # first, raise an argument error when self and sq have the same coordinates
     if x_difference.zero? && y_difference.zero?
-      raise ArgumentError, 'location to check has to be different'
+      raise ArgumentError, 'destination has to have a different location'
     elsif x_difference.zero?
       # the two locations are on the same vertical line
       # is there a chess piece on the same vertical line AND sits in between these two locations?
@@ -19,19 +19,19 @@ class Piece < ApplicationRecord
       # the y_coordinates of the this chess piece should be in between (self.y_coordinate to sq[1])
 
       @chess_in_between = Piece.where(x_coordinate: x_coordinate)
-                               .where(y_coordinate: y_coordinate + 1...sq[1])
+                               .where(y_coordinate: y_coordinate + 1...destination_y)
     elsif y_difference.zero? # the two locations are on the same horizontal line
       @chess_in_between = Piece.where(y_coordinate: y_coordinate)
-                               .where(x_coordinate: x_coordinate + 1...sq[0])
+                               .where(x_coordinate: x_coordinate + 1...destination_x)
     elsif x_difference != y_difference
-      raise ArgumentError, 'x and y difference are not the same'
+      raise ArgumentError, 'destination must be on the diagonal of the origin'
     else # the two locations are on the same diagonal line
       # three conditions to check:
       # is the x_coord of the chess piece in between self.x_coordinate and sq[0]
       # is the y_coord of the chess piece in between self.y_coordinate and sq[1]
       # if the two conditions above are all satisfied, we also want to check
-      @chess_in_between = Piece.where(x_coordinate: x_coordinate + 1...sq[0]) # making sure the x_coord of the chess piece in between self.x_coordinate and sq[0]
-                               .where(y_coordinate: y_coordinate + 1...sq[1]) # making sure the y_coord of the chess piece in between self.y_coordinate and sq[1]
+      @chess_in_between = Piece.where(x_coordinate: x_coordinate + 1...destination_x) # making sure the x_coord of the chess piece in between self.x_coordinate and sq[0]
+                               .where(y_coordinate: y_coordinate + 1...destination_y) # making sure the y_coord of the chess piece in between self.y_coordinate and sq[1]
                                .where("(x_coordinate - #{x_coordinate}) = (y_coordinate- #{y_coordinate})")
 
     end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -22,19 +22,17 @@ RSpec.describe Piece, type: :model do
       describe 'Should raise an argument error' do
         it 'If current_chess_piece and sq2 have the same x and y coordinates' do
           current_chess_piece = create_chess(0, 1)
-
-          sq2 = [0, 1]
-          # assertion
-          expect { current_chess_piece.obstructed?(sq2) }.to raise_error ArgumentError
+          destination_coord = { destination_x: 0, destination_y: 1 }
+    
+          expect { current_chess_piece.obstructed?(destination_coord[:destination_x], destination_coord[:destination_y]) }.to raise_error ArgumentError
         end
 
         describe 'If current_chess_piece and sq2 have different x and y coordinates' do
           it 'and the x differences and y differences are not the same' do
             current_chess_piece = create_chess(0, 5)
-
-            sq2 = [1, 2]
-            # assertion
-            expect { current_chess_piece.obstructed?(sq2) }.to raise_error ArgumentError
+            destination_coord = { destination_x: 1, destination_y: 2 }
+        
+            expect { current_chess_piece.obstructed?(destination_coord[:destination_x], destination_coord[:destination_y]) }.to raise_error ArgumentError
           end
         end
       end
@@ -45,20 +43,19 @@ RSpec.describe Piece, type: :model do
         describe 'but not the same y coordinates' do
           it 'If there is a piece in between them, the result should be true' do
             current_chess_piece = create_chess(3, 3)
-
-            sq2 = [3, 5]
+            destination_coord = { destination_x: 3, destination_y: 5 }
 
             # create a chess in-between
             create_chess(3, 4)
 
-            expect(current_chess_piece.obstructed?(sq2)).to eq true
+            expect(current_chess_piece.obstructed?(destination_coord[:destination_x], destination_coord[:destination_y])).to eq true
           end
 
           it 'If there is nothing in between them, the result should be false' do
             current_chess_piece = create_chess(3, 3)
-            sq2 = [3, 5]
+            destination_coord = { destination_x: 3, destination_y: 5 }
 
-            expect(current_chess_piece.obstructed?(sq2)).to eq false
+            expect(current_chess_piece.obstructed?(destination_coord[:destination_x], destination_coord[:destination_y])).to eq false
           end
         end
       end
@@ -67,19 +64,19 @@ RSpec.describe Piece, type: :model do
         describe 'but not the same x coordinates' do
           it 'If there is a piece in between them, the result should be true' do
             current_chess_piece = create_chess(3, 3)
+            destination_coord = { destination_x: 5, destination_y: 3 }
 
-            sq2 = [5, 3]
             # create a chess in-between
             create_chess(4, 3)
 
-            expect(current_chess_piece.obstructed?(sq2)).to eq true
+            expect(current_chess_piece.obstructed?(destination_coord[:destination_x], destination_coord[:destination_y])).to eq true
           end
 
           it 'If there is nothing in between them, the result should be false' do
-            sq2 = [5, 3]
             current_chess_piece = create_chess(3, 3)
+            destination_coord = { destination_x: 5, destination_y: 3 }
 
-            expect(current_chess_piece.obstructed?(sq2)).to eq false
+            expect(current_chess_piece.obstructed?(destination_coord[:destination_x], destination_coord[:destination_y])).to eq false
           end
         end
       end
@@ -88,18 +85,19 @@ RSpec.describe Piece, type: :model do
         describe 'and the x differences and y differences are the same' do
           it 'if there is a piece in between them, the result should be true' do
             current_chess_piece = create_chess(0, 0)
-            sq2 = [3, 3]
+            destination_coord = { destination_x: 3, destination_y: 3 }
+            
             # create a chess in-between
             create_chess(1, 1)
 
-            expect(current_chess_piece.obstructed?(sq2)).to eq true
+            expect(current_chess_piece.obstructed?(destination_coord[:destination_x], destination_coord[:destination_y])).to eq true
           end
 
           it 'if there is nothing in between them, the result should be false' do
             current_chess_piece = create_chess(0, 0)
-            sq2 = [3, 3]
+            destination_coord = { destination_x: 3, destination_y: 3 }
 
-            expect(current_chess_piece.obstructed?(sq2)).to eq false
+            expect(current_chess_piece.obstructed?(destination_coord[:destination_x], destination_coord[:destination_y])).to eq false
           end
         end
       end
