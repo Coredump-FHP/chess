07ffd6a53310b73feee3730bfef869f2d274cb81
waders92 <wade.carlson@pga.com>
2017-02-17 17:56:41 -0600

    Added get: request for new test and expect response to have success

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -3,16 +3,23 @@ require 'pry'
 
 RSpec.describe GamesController, type: :controller do
   describe 'games#new action' do
-    it 'should login players' do
+    it 'should login players and create a new game form' do
       player = FactoryGirl.create(:player)
       sign_in player
+
+      get :new
+      expect(response).to have_http_status(:success)
     end
   end
 
   describe 'games#create action' do
     it 'should create a new game in the database' do
+      player = FactoryGirl.create(:player)
+      sign_in player
+
       post :create, params: { game: { name: 'test game' } }
       expect(response).to redirect_to root_path
+
       @game = Game.last
       expect(@game.name).to eq 'test game'
     end
