cefb346ddb22cfe2f068c030d5208673ccacab1d
sylyu <sylviah.yu@gmail.com>
2017-02-17 02:37:00 +0000

    unrefactored the code, and added missing columns to game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,89 +4,177 @@ class Game < ApplicationRecord
   belongs_to :player_1, class_name: 'Player', optional: true
   belongs_to :player_2, class_name: 'Player', optional: true
   belongs_to :winning_player, class_name: 'Player', optional: true
-  # Is called after  Base.save on new objects that have not been saved yet (no record exists).
-  after_create :populate_game!
-  # How to fix messed up migrations http://stackoverflow.com/questions/20082002/migrations-are-pending-run-bin-rake-dbmigrate-rails-env-development-to-resol
   # Populate a game with all the pieces in the correct locations (x_coordinate, y_coordinate)
-
   def populate_game!
-    add_starting_pieces_for_color('white')
-    add_starting_pieces_for_color('black')
-  end
-
-  def add_starting_pieces_for_color(_color)!
+    # white player
     (1..8).each do |i|
       pieces << Piece.create(
         game: self,
         player: player_1,
+        color: 'white',
         x_coordinate: i,
         y_coordinate: 2,
-        type: 'Pawn'
+        type: 'Pawn',
+        captured: false
       )
     end
-                                           pieces << Piece.create(
-                                             game: self,
-                                             player: player_1,
-                                             x_coordinate: 1,
-                                             y_coordinate: 1,
-                                             type: 'Rook'
-                                           )
-
-                                           pieces << Piece.create(
-                                             game: self,
-                                             player: player_1,
-                                             x_coordinate: 8,
-                                             y_coordinate: 1,
-                                             type: 'Rook'
-                                           )
-
-                                           pieces << Piece.create(
-                                             game: self,
-                                             player: player_1,
-                                             x_coordinate: 2,
-                                             y_coordinate: 1,
-                                             type: 'Knight'
-                                           )
-
-                                           pieces << Piece.create(
-                                             game: self,
-                                             player: player_1,
-                                             x_coordinate: 7,
-                                             y_coordinate: 1,
-                                             type: 'Knight'
-                                           )
-                                           pieces << Piece.create(
-                                             game: self,
-                                             player: player_1,
-                                             x_coordinate: 3,
-                                             y_coordinate: 1,
-                                             type: 'Bishop'
-                                           )
-
-                                           pieces << Piece.create(
-                                             game: self,
-                                             player: player_1,
-                                             x_coordinate: 6,
-                                             y_coordinate: 1,
-                                             type: 'Bishop'
-                                           )
-
-                                           pieces << Piece.create(
-                                             game: self,
-                                             player: player_1,
-                                             x_coordinate: 4,
-                                             y_coordinate: 1,
-                                             type: 'Queen'
-                                           )
-
-                                           pieces << Piece.create(
-                                             game: self,
-                                             player: player_1,
-                                             x_coordinate: 5,
-                                             y_coordinate: 1,
-                                             type: 'King'
-                                           )
-                                           save
-                                           self
+    pieces << Piece.create(
+      game: self,
+      player: player_1,
+      color: 'white',
+      x_coordinate: 1,
+      y_coordinate: 1,
+      type: 'Rook',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_1,
+      color: 'white',
+      x_coordinate: 2,
+      y_coordinate: 1,
+      type: 'Knight',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_1,
+      color: 'white',
+      x_coordinate: 3,
+      y_coordinate: 1,
+      type: 'Bishop',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_1,
+      color: 'white',
+      x_coordinate: 4,
+      y_coordinate: 1,
+      type: 'Queen',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_1,
+      color: 'white',
+      x_coordinate: 5,
+      y_coordinate: 1,
+      type: 'King',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_1,
+      color: 'white',
+      x_coordinate: 6,
+      y_coordinate: 1,
+      type: 'Bishop',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_1,
+      color: 'white',
+      x_coordinate: 7,
+      y_coordinate: 1,
+      type: 'Knight',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_1,
+      color: 'white',
+      x_coordinate: 8,
+      y_coordinate: 1,
+      type: 'Rook',
+      captured: false
+    )
+    # black player
+    (1..8).each do |i|
+      pieces << Piece.create(
+        game: self,
+        player: player_2,
+        color: 'black',
+        x_coordinate: i,
+        y_coordinate: 7,
+        type: 'Pawn',
+        captured: false
+      )
+    end
+    pieces << Piece.create(
+      game: self,
+      player: player_2,
+      color: 'black',
+      x_coordinate: 1,
+      y_coordinate: 8,
+      type: 'Rook',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_2,
+      color: 'black',
+      x_coordinate: 2,
+      y_coordinate: 8,
+      type: 'Knight',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_2,
+      color: 'black',
+      x_coordinate: 3,
+      y_coordinate: 8,
+      type: 'Bishop',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_2,
+      color: 'black',
+      x_coordinate: 4,
+      y_coordinate: 8,
+      type: 'Queen',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_2,
+      color: 'black',
+      x_coordinate: 5,
+      y_coordinate: 8,
+      type: 'King',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_2,
+      color: 'black',
+      x_coordinate: 6,
+      y_coordinate: 8,
+      type: 'Bishop',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_2,
+      color: 'black',
+      x_coordinate: 7,
+      y_coordinate: 8,
+      type: 'Knight',
+      captured: false
+    )
+    pieces << Piece.create(
+      game: self,
+      player: player_2,
+      color: 'black',
+      x_coordinate: 8,
+      y_coordinate: 8,
+      type: 'Rook',
+      captured: false
+    )
+    save
+    self
   end
 end
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,9 +1,5 @@
 class Piece < ApplicationRecord
   belongs_to :player, class_name: 'Player', optional: true
   belongs_to :game
-
-  # we should not name a column "type", but this gets around the problem
-  # http://apidock.com/rails/ActiveRecord/Base/inheritance_column/class
-  # disable STI
-  enum color: %w(white black)
+  # enum color: %w(white black)
 end
