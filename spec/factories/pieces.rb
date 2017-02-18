0638d4c0b2eac3bcb4d7a7d0533de9bdbe089c1b
Sijia Davis <arwen@ksu.edu>
2017-02-18 23:45:08 +0000

    Fixing missing FactoryGirl syntax...:

--- spec/factories/pieces.rb
+++ spec/factories/pieces.rb
@@ -7,50 +7,50 @@ FactoryGirl.define do
     captured false
 
     factory :pawn do
-     player 'player_1'
-     color 'white'
-     x_coordinate 2
-     y_coordinate 2
-     type 'Pawn'
-   end
+      player 'player_1'
+      color 'white'
+      x_coordinate 2
+      y_coordinate 2
+      type 'Pawn'
+    end
 
-   factory :queen do
-     player 'player_1'
-     color 'white'
-     x_coordinate 4
-     y_coordinate 1
-     type 'Queen'
-   end
+    factory :queen do
+      player 'player_1'
+      color 'white'
+      x_coordinate 4
+      y_coordinate 1
+      type 'Queen'
+    end
 
-   factory :knight do
-     player 'player_1'
-     color 'white'
-     x_coordinate 2
-     y_coordinate 1
-     type 'Knight'
-   end
+    factory :knight do
+      player 'player_1'
+      color 'white'
+      x_coordinate 2
+      y_coordinate 1
+      type 'Knight'
+    end
 
-   factory :bishop do
-     player 'player_1'
-     color 'white'      
-     x_coordinate 3
-     y_coordinate 1
-     type 'Bishop'
-   end
+    factory :bishop do
+      player 'player_1'
+      color 'white'      
+      x_coordinate 3
+      y_coordinate 1
+      type 'Bishop'
+    end
 
-   factory :rook do
-     player 'player_2'
-     color 'black'
-     x_coordinate 1
-     y_coordinate 1
-     type 'Rook'
-   end
+    factory :rook do
+      player 'player_2'
+      color 'black'
+      x_coordinate 1
+      y_coordinate 1
+      type 'Rook'
+    end
 
-   factory :king do
-     color 'white'
-     x_coordinate 5
-     y_coordinate 1
-     type 'King'
-   end
+    factory :king do
+      color 'white'
+      x_coordinate 5
+      y_coordinate 1
+      type 'King'
+    end
   end
 end
