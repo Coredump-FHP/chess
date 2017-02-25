9b9e5aefd6a29ca916256b5faa369715bb77e0ec
Sijia Davis <arwen@ksu.edu>
2017-02-25 17:59:14 +0000

    Fix syntax

--- app/models/player.rb
+++ app/models/player.rb
@@ -16,7 +16,7 @@ class Player < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |player|
-      if data == session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
         player.email = data['email'] if player.email.blank?
       end
     end
