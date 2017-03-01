9adfa29c8268129a3d1fe882a1c6f468a2e21d79
waders92 <wade.carlson@pga.com>
2017-03-01 14:27:51 -0600

    all but one test passes for pawn, will attempt to fix shorly

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,2 +1,44 @@
 class Pawn < Piece
+  def legal_move?(x, y)
+    return false if backwards_move? y
+    return false if horizontal_move? x
+    return true if capture_move?(x,y)
+    return false if game.obstructed?(x,y)
+    return true if first_move?(y)
+
+    proper_length?(y)
+  end
+
+  def capture_move?(x, y)
+    x_diff = (x_coordinate - x).abs
+    y_diff = (y_coordinate - y).abs
+    captured_piece = piece.obstructed?(x, y)
+
+    return false if captured_piece.blank?
+    return false if captured_piece.color == color
+    return true if x_diff == 1 && y_diff == 1
+  end
+
+  def horizontal_move?(x)
+    x_diff = (x_coordinate - x).abs
+    x_diff != 0
+  end
+
+  def backwards_move?(y)
+    color ? y_coordinate > y : y_coordinate < y
+  end
+
+  def first_move?(x, y)
+    (y_coordinate == 1 && color) || (y_coordinate == 6 && !color)
+    y_diff = (y_coordinate - y).abs
+    if y_diff == 1 || y_diff == 2
+      return true
+    else
+      return false
+    end
+  end
+
+  def proper_length?(y)
+    (y - y_coordinate).abs == 1
+  end
 end
---
+++ spec/models/pawn_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe Pawn, type: :model do
+  describe 'legal_move?' do
+    it 'can move up to 2 spaces on the first move' do
+      pawn = Pawn.create(x_coordinate: 1, y_coordinate: 1)
+
+      expect(pawn.first_move?(1, 2)).to eq true
+      expect(pawn.first_move?(1, 3)).to eq true
+      expect(pawn.first_move?(1, 6)).to eq false
+    end
+  end
+
+  describe 'legal_move?' do
+    it 'cannot move backwards' do
+      pawn = Pawn.create(x_coordinate: 1, y_coordinate: 1)
+      expect(pawn.backwards_move?(0)).to eq false
+    end
+  end
+
+  describe 'legal_move?' do
+    it 'cannot move horizontally on the board' do
+      pawn = Pawn.create(x_coordinate: 1, y_coordinate: 1)
+      expect(pawn.horizontal_move?(0)).to eq true
+      expect(pawn.horizontal_move?(4)).to eq true
+    end
+  end
+
+  describe 'legal_move?' do
+    it 'can capture a piece' do
+      pawn = Pawn.create(x_coordinate: 1, y_coordinate: 1)
+      pawn_to_capture = Pawn.create(x_coordinate: 2, y_coordinate: 2)
+
+      expect(pawn.capture_move?(2, 2)).to eq true
+      expect(pawn.capture_move?(3, 3)).to eq false
+    end
+  end
+
+  describe 'legal_move?' do
+    it 'has a proper_length move of 1 (y) square at a time' do
+      pawn = Pawn.create(x_coordinate: 1, y_coordinate: 1)
+
+      expect(pawn.proper_length?(2)).to eq true
+      expect(pawn.proper_length?(4)).to eq false
+    end
+  end
+end
