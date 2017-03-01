b32261be7366d1cab6fce4e0bab405da981a81ae
waders92 <wade.carlson@pga.com>
2017-03-01 15:02:34 -0600

    Not sure how to fix this last rubocop issue....

--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -2,13 +2,12 @@ module PiecesHelper
   def render_piece_highlight(x, y)
     piece = @game.retrieve_piece(x, y)
     return unless piece.present?
-      if piece == @piece
-        return content_tag(:div, image_tag(piece.icon), class: 'highlight')
-      else
-        return image_tag(piece.icon)
-      end
+    if piece == @piece
+      return content_tag(:div, image_tag(piece.icon), class: 'highlight')
     else
-      return link_to(raw(content_tag(:div, ' ', class: 'clickable-square')), piece_path(@piece, x_coordinate: x, y_coordinate: y), method: 'PUT')
+      return image_tag(piece.icon)
+    end
+    link_to(raw(content_tag(:div, ' ', class: 'clickable-square')), piece_path(@piece, x_coordinate: x, y_coordinate: y), method: 'PUT')
     end
   end
 end
