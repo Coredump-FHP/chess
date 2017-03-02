fc5d610047f4ca5774f4955a9aa2121d7216f16c
waders92 <wade.carlson@pga.com>
2017-03-02 15:21:05 -0600

    fixed styling error, and unreachable code error

--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -2,12 +2,7 @@ module PiecesHelper
   def render_piece_highlight(x, y)
     piece = @game.retrieve_piece(x, y)
     return unless piece.present?
-    if piece == @piece
-      return content_tag(:div, image_tag(piece.icon), class: 'highlight')
-    else
-      return image_tag(piece.icon)
-    end
-    link_to(raw(content_tag(:div, ' ', class: 'clickable-square')), piece_path(@piece, x_coordinate: x, y_coordinate: y), method: 'PUT')
-    end
+    return content_tag(:div, image_tag(piece.icon), class: 'highlight') if piece == @piece
+    image_tag(piece.icon)
   end
 end
