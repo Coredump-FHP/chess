3d7d46edad2b6463f6030733a134c156f9c71ca9
Sijia Davis <arwen@ksu.edu>
2017-03-06 11:04:40 +0000

    Refactor some of the code in the controllers

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,18 +1,22 @@
 class PiecesController < ApplicationController
   helper PiecesHelper
+  before_action :find_piece
+  
   def show
-    @piece = Piece.find(params[:id])
     @game = @piece.game
   end
 
   def update
-    @piece = Piece.find(params[:id])
     @piece.update_attributes(piece_params)
     @game = @piece.game
     render template: 'games/show'
   end
 
   private
+  
+  def find_piece
+    @piece = Piece.find(params[:id])
+  end
 
   def piece_params
     params.require(:piece).permit(:x_coordinate, :y_coordinate)
