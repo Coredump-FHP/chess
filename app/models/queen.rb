092be40aedd27cc466eb3edb8ee7c10a60d43d8d
Elson Mathew <Mathew@Elsons-MacBook-Pro.local>
2017-03-05 21:02:03 -0500

    Remove obstructed method

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,7 +1,6 @@
 class Queen < Piece
   def valid_move?(destination_x, destination_y)
     return false unless on_board?(destination_x, destination_y)
-    return false if obstructed?(destination_x, destination_y)
 
     x_diff = (x_coordinate - destination_x).abs
     y_diff = (y_coordinate - destination_y).abs
